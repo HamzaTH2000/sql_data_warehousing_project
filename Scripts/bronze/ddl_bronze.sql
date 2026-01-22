2c3f11b0e1603cae8bcebd320b962dfe1c0367c8
Hamza Thamlaoui <hamzathamleoui@gmail.com>
2026-01-22 10:46:39 +0100

    Create ddl_bronze.sql

---
+++ Scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,104 @@
+
+-- ============================================================================
+-- Bronze Layer - Raw Data Table Definitions
+-- ============================================================================
+
+
+
+
+/* This script creates 6 empty tables in a "bronze" schema (raw data layer) to store data from two source systems: 
+CRM (3 tables for customers, products, and sales) and ERP (3 tables for location, customer demographics, and product categories). 
+It drops any existing versions of these tables before creating new ones.*/
+
+
+
+
+-- ============================================================================
+-- CRM SYSTEM TABLES
+-- ============================================================================
+
+
+
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL -- to drop any existing table before creating new one.
+    DROP TABLE bronze.crm_cust_info;
+
+CREATE TABLE bronze.crm_cust_info (
+cst_id INT,
+cst_key NVARCHAR (50),
+cst_firstname NVARCHAR (50),
+cst_lastname NVARCHAR (50),
+cst_marital_status NVARCHAR (50),
+cst_gender NVARCHAR (50),
+cst_create_date DATE
+);
+
+
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL 
+    DROP TABLE bronze.crm_prd_info;
+
+CREATE TABLE bronze.crm_prd_info (
+prd_id INT,
+prd_key NVARCHAR (50),
+prd_nm NVARCHAR (50),
+prd_cost INT,
+prd_line NVARCHAR (50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
+);
+
+
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL 
+    DROP TABLE bronze.crm_sales_details;
+
+CREATE TABLE bronze.crm_sales_details (
+sls_ord_num NVARCHAR (50),
+sls_prd_key NVARCHAR (50),
+sls_cust_id INT,
+sls_order_dt INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
+
+
+
+
+-- ============================================================================
+-- ERP SYSTEM TABLES
+-- ============================================================================
+
+
+
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL 
+    DROP TABLE bronze.erp_loc_a101;
+
+CREATE TABLE bronze.erp_loc_a101 (
+cid NVARCHAR (50),
+cntry NVARCHAR (50)
+);
+
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL 
+    DROP TABLE bronze.erp_cust_az12;
+
+CREATE TABLE bronze.erp_cust_az12 (
+cid NVARCHAR (50),
+bdate DATE,
+gen NVARCHAR (50)
+);
+
+
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL 
+    DROP TABLE bronze.erp_px_cat_g1v2;
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+id NVARCHAR (50),
+cat NVARCHAR (50),
+subcat NVARCHAR (50),
+maintenance NVARCHAR (50)
+);
+
+
+
+-- NEXT STEP: Load CSV data from source systems into these bronze layer tables
